fdcb16a5bdebfcf28bdd5488311c3cc1bbc8af91
Jac22 <jacopo.prudentino@gmail.com>
2023-02-03 13:41:07 +0100

    Creata route per la pagina 'home'

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Saint;
+
+class MainController extends Controller
+{
+    public function Home(){
+        $saints = Saint::all();
+        $data = [
+            'saints' => $saints,
+        ];
+        return view('pages.home', $data);
+    }
+}
